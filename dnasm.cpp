94d4d70c349d31ac652a7ed209eac158a65e09e2
Sarraf, Dara <90422816+dora-explora@users.noreply.github.com>
2024-05-18 03:06:18 +0000

    made it work basically

--- dnasm.cpp
+++ dnasm.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-}
+    char input;
+    string codons;
+    bool keepgoing = true;
+    while (keepgoing) {
+        cin >> input;
+        switch (input) {
+            case 'd':
+                codons += "00";
+                cout << codons << endl;
+                break;
+            case 'f':
+                codons += "01";
+                cout << codons << endl;
+                break;
+            case 'j':
+                codons += "10";
+                cout << codons << endl;
+                break;
+            case 'k':
+                codons += "11";
+                cout << codons << endl;
+                break;
+            case 'x':
+                cout << "final code: " << codons << endl;
+                keepgoing = false;
+                break;
+            }
+    }
+    cout << "loop done!";
+};
