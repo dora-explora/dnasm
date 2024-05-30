2c05818c79b9c9c5cdfc578a6b182d6cf0a1d591
Sarraf, Dara <90422816+dora-explora@users.noreply.github.com>
2024-05-30 18:23:48 +0000

    autocommit

--- dnasm.cpp
+++ dnasm.cpp
@@ -191,8 +191,8 @@ int main() {
             readiedmarkers.push_back(codons[i]);
         } else if (decodons[i] == "RunPro") {
             workingcursor |= codons[i+1] << 18;
-            workingcursor |= codons[i+2] << 12;
-            workingcursor |= codons[i+3] << 6;
+            workingcursor |= codons[i+2] << 16;
+            workingcursor |= codons[i+3] << 8;
             workingcursor |= codons[i+4];
             for (int j = 0; j < readiedmarkers.size(); j++) {
                 enzymes.at(readiedmarkers[i]).cursor = workingcursor;
